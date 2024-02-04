310c63362fd7a230a3713bf788444eda0b1130c3
USER <USER@DESKTOP-SD>
2024-02-04 23:13:48 +0530

    update commit

--- ApiBasedSSO/src/main/java/com/writer/cymmetri/apibasedsso/config/WebMvcConfig.java
+++ ApiBasedSSO/src/main/java/com/writer/cymmetri/apibasedsso/config/WebMvcConfig.java
@@ -1,14 +1,14 @@
 package com.writer.cymmetri.apibasedsso.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/api/users/login/**")
+                .addResourceLocations("classpath:/static/api/users/login/");
     }
 }
